36c521e3df2d0fdbad1ee0c63ca6d4f52c98cd71
zetroot <zetroot@gmail.com>
2021-07-11 22:42:11 +0300

    added bizlayer abstractions project

---
+++ src/SafeHouseCRM.BizLayer.Abstractions/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SafeHouseCRM.BizLayer.Abstractions.Models
+{
+    /// <summary>
+    /// Карточка пострадавшего
+    /// </summary>
+    public class Person : IBizRecord
+    {
+        /// <summary>
+        /// Идентификатор записи
+        /// </summary>
+        public Guid ID { get; set; }
+        
+        /// <summary>
+        /// Признак удалённой записи
+        /// </summary>
+        public bool IsDeleted { get; set; }
+        
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
