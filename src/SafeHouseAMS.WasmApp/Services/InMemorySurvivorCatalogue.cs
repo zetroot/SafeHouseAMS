8b340390662206020161bd0567f59556f4f097c9
zetroot <zetroot@gmail.com>
2021-07-12 23:49:30 +0300

    added validation of survivor name

--- src/SafeHouseAMS.WasmApp/Services/InMemorySurvivorCatalogue.cs
+++ src/SafeHouseAMS.WasmApp/Services/InMemorySurvivorCatalogue.cs
@@ -16,16 +16,15 @@ namespace SafeHouseAMS.WasmApp.Services
         {
             new()
             {
-                ID = Guid.NewGuid(), Name = "Boris", Surname = "Britva", BirthDate = DateTime.Today, IsDeleted = false
+                ID = Guid.NewGuid(), Name = "Boris", BirthDateAccurate = new DateTime(1980, 05, 09),
+                Num = 5
             },
             new()
             {
-                ID = Guid.NewGuid(), Name = "Boris", Surname = "Hren popadesh", BirthDate = DateTime.Today, IsDeleted = true
+                ID = Guid.NewGuid(), Name = "Boris", BirthDateCalculated = new DateTime(1970, 01, 01),
+                Num = 7
             },
-            new()
-            {
-                ID = Guid.NewGuid(), Name = "Boris", Surname = "Kot", BirthDate = DateTime.Today, IsDeleted = false
-            }
+            new() {ID = Guid.NewGuid(), Name = "Boris", Num = 9}
         };
         
         public async IAsyncEnumerable<Survivor> GetCollection()
@@ -40,22 +39,21 @@ namespace SafeHouseAMS.WasmApp.Services
         public Task<Survivor> Add(Survivor adding)
         {
             adding.ID = Guid.NewGuid();
+            adding.Num = _store.Select(x => x.Num).Max() + 1;
             _store.Add(adding);
             return Task.FromResult(adding);
         }
 
         public Task<Survivor> Update(Survivor updating)
         {
-            if (_store.Any(x => x.ID == updating.ID))
-            {
-                var found = _store.Single(x => x.ID == updating.ID);
-                found.Name = updating.Name;
-                found.BirthDate = updating.BirthDate;
-                found.Surname = updating.Surname;
-                return Task.FromResult(found);
-            }
-
-            throw new InvalidOperationException();
+            if (!_store.Any(x => x.ID == updating.ID)) 
+                throw new InvalidOperationException();
+            
+            var found = _store.Single(x => x.ID == updating.ID);
+            found.Name = updating.Name;
+            found.BirthDateAccurate = updating.BirthDateAccurate;
+            found.BirthDateCalculated = updating.BirthDateCalculated;
+            return Task.FromResult(found);
         }
 
         public Task Delete(Guid id)
