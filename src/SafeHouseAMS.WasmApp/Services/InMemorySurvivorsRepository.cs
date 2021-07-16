65489b36428da6260507954838919f33a76c872e
zetroot <zetroot@gmail.com>
2021-07-16 13:55:49 +0300

    generate fake data with Fake.net

--- src/SafeHouseAMS.WasmApp/Services/InMemorySurvivorsRepository.cs
+++ src/SafeHouseAMS.WasmApp/Services/InMemorySurvivorsRepository.cs
@@ -9,28 +9,44 @@ namespace SafeHouseAMS.WasmApp.Services
 {
     public class InMemorySurvivorsRepository : ISurvivorRepository
     {
-        private readonly List<Survivor> _store = new()
+        private readonly List<Survivor> _store;
+        // {
+        //     new(Guid.NewGuid(), false, DateTimeOffset.Now, DateTimeOffset.Now, "Boris Britva",
+        //         3,
+        //         SexEnum.Male, null, 
+        //         new(1970, 01 ,01, 0,0,0, TimeSpan.FromHours(3)), null),
+        //     
+        //     new(Guid.NewGuid(), false, DateTimeOffset.Now, DateTimeOffset.Now, "Кошка Блоха",
+        //         5,
+        //         SexEnum.Female, null, 
+        //         null, new(2007, 06 ,15, 0,0,0, TimeSpan.FromHours(3))),
+        //     
+        //     new(Guid.NewGuid(), false, DateTimeOffset.Now, DateTimeOffset.Now, "Паук Например",
+        //         6,
+        //         SexEnum.Other, null, 
+        //         null, null),
+        //     
+        //     new(Guid.NewGuid(), false, DateTimeOffset.Now, DateTimeOffset.Now, "Неустановленное лицо",
+        //         42,
+        //         SexEnum.Other, "не установлен", 
+        //         null, null),
+        // };
+
+        public InMemorySurvivorsRepository()
         {
-            new(Guid.NewGuid(), false, DateTimeOffset.Now, DateTimeOffset.Now, "Boris Britva",
-                3,
-                SexEnum.Male, null, 
-                new(1970, 01 ,01, 0,0,0, TimeSpan.FromHours(3)), null),
-            
-            new(Guid.NewGuid(), false, DateTimeOffset.Now, DateTimeOffset.Now, "Кошка Блоха",
-                5,
-                SexEnum.Female, null, 
-                null, new(2007, 06 ,15, 0,0,0, TimeSpan.FromHours(3))),
-            
-            new(Guid.NewGuid(), false, DateTimeOffset.Now, DateTimeOffset.Now, "Паук Например",
-                6,
-                SexEnum.Other, null, 
-                null, null),
-            
-            new(Guid.NewGuid(), false, DateTimeOffset.Now, DateTimeOffset.Now, "Неустановленное лицо",
-                42,
-                SexEnum.Other, "не установлен", 
-                null, null),
-        }; 
+            _store = Enumerable
+                .Range(42, 200)
+                .Select(x =>
+                {
+                    var lastEdit = DateTimeOffset.Now - TimeSpan.FromHours(Faker.RandomNumber.Next(24, 24000));
+                    var created = lastEdit - TimeSpan.FromHours(Faker.RandomNumber.Next(24, 24000));
+                    var dob = DateTime.Today - TimeSpan.FromDays(Faker.RandomNumber.Next(7000, 19000));
+                    return new Survivor(Guid.NewGuid(), Faker.Boolean.Random(), created, lastEdit,
+                        Faker.Name.FullName(), x, Faker.Enum.Random<SexEnum>(), Faker.Lorem.GetFirstWord(), dob, null);
+                })
+                .ToList();
+        }
+
         public Task Create(Guid id, bool isDeleted, DateTimeOffset created, DateTimeOffset lastEdit, string name, SexEnum sex,
             string? otherSex, DateTimeOffset? accurateDob, DateTimeOffset? calculatedDob)
         {
