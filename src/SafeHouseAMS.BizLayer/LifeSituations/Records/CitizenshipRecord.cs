3e8d689afb7f7f79a659103c0fad1ba0f26959fd
zetroot <zetroot@gmail.com>
2021-07-17 20:38:40 +0300

    added life situation records

---
+++ src/SafeHouseAMS.BizLayer/LifeSituations/Records/CitizenshipRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SafeHouseAMS.BizLayer.LifeSituations.Records
+{
+    /// <summary>
+    /// Запись о гражданстве
+    /// </summary>
+    public class CitizenshipRecord : BaseRecord
+    {
+        /// <summary>
+        /// Гражданство
+        /// </summary>
+        public string Citizenship { get; }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="id">идентификатор записи</param>
+        /// <param name="citizenship">гражданство</param>
+        /// <exception cref="ArgumentNullException">если передан null</exception>
+        public CitizenshipRecord(Guid id, string citizenship) : base(id)
+        {
+            Citizenship = citizenship ?? throw new ArgumentNullException(nameof(citizenship));
+        }
+    }
+}
