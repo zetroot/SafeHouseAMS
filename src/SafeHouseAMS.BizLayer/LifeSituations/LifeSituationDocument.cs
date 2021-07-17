7b5e3c83c3283bf37d31ab3e31fe72d3fbbdb82b
zetroot <zetroot@gmail.com>
2021-07-17 20:37:41 +0300

    added document model

---
+++ src/SafeHouseAMS.BizLayer/LifeSituations/LifeSituationDocument.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SafeHouseAMS.BizLayer.Survivors;
+
+namespace SafeHouseAMS.BizLayer.LifeSituations
+{
+    /// <summary>
+    /// Базовый класс документов, меняющих жизненную ситуацию
+    /// </summary>
+    public abstract class LifeSituationDocument : BaseDomainModel
+    {
+        /// <summary>
+        /// Дата документа
+        /// </summary>
+        public DateTimeOffset DocumentDate { get; }
+
+        /// <summary>
+        /// Пострадавший к которому относится эта запись
+        /// </summary>
+        public Survivor Survivor { get; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="id">Идентификатор записи</param>
+        /// <param name="isDeleted">признак удалённой записи</param>
+        /// <param name="created">дата создания</param>
+        /// <param name="lastEdit">дата последнего редактирования</param>
+        /// <param name="documentDate">дата действия документа</param>
+        /// <param name="survivor">Пострадавший, к которому относится этот документ</param>
+        protected LifeSituationDocument(Guid id, bool isDeleted, DateTimeOffset created, DateTimeOffset lastEdit, DateTimeOffset documentDate, Survivor survivor) :
+            base(id, isDeleted, created, lastEdit)
+        {
+            DocumentDate = documentDate;
+            Survivor = survivor;
+        }
+    }
+}
