c7d652034f2b73a4bde54206aa1c73c8980bbd76
zetroot <zetroot@gmail.com>
2021-07-17 20:39:11 +0300

    added inquiry sources

---
+++ src/SafeHouseAMS.BizLayer/LifeSituations/InquirySources/ForwardedBySurvivor.cs
@@ -0,0 +1,16 @@
+﻿namespace SafeHouseAMS.BizLayer.LifeSituations.InquirySources
+{
+    /// <summary>
+    /// Перенаправлен другим пострадавшим
+    /// </summary>
+    public class ForwardedBySurvivor : ForwardedInquiry
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="forwardedBy">Перенаправивший пострадавший</param>
+        public ForwardedBySurvivor(string forwardedBy) : base(forwardedBy)
+        {
+        }
+    }
+}
---
+++ src/SafeHouseAMS.BizLayer/LifeSituations/InquirySources/ForwardedInquiry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SafeHouseAMS.BizLayer.LifeSituations.InquirySources
+{
+    /// <summary>
+    /// Общий класс перенаправленного обращения
+    /// </summary>
+    public abstract class ForwardedInquiry : IInquirySource
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="forwardedBy">кем перенаправлен - название организации или человек</param>
+        protected ForwardedInquiry(string forwardedBy)
+        {
+            ForwardedBy = forwardedBy ?? throw new ArgumentNullException(nameof(forwardedBy));
+        }
+
+        /// <summary>
+        /// Источник перенаправления - организация, человек
+        /// </summary>
+        public string ForwardedBy { get; }
+    }
+
+}
