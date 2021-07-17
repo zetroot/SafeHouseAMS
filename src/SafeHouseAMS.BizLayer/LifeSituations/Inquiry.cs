5be18eac64f676ed6143d5a2301661938c5438c6
zetroot <zetroot@gmail.com>
2021-07-17 20:40:36 +0300

    added Inquiry document

---
+++ src/SafeHouseAMS.BizLayer/LifeSituations/Inquiry.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SafeHouseAMS.BizLayer.LifeSituations.InquirySources;
+using SafeHouseAMS.BizLayer.LifeSituations.Records;
+using SafeHouseAMS.BizLayer.LifeSituations.Vulnerabilities;
+using SafeHouseAMS.BizLayer.Survivors;
+
+namespace SafeHouseAMS.BizLayer.LifeSituations
+{
+    /// <summary>
+    /// Обращение за помощью
+    /// </summary>
+    public class Inquiry : LifeSituationDocument
+    {
+        /// <summary>
+        /// несовершеннолетний на момент обращения
+        /// </summary>
+        public bool IsJuvenile { get; }
+
+        /// <summary>
+        /// Источники обращения
+        /// </summary>
+        public IReadOnlyCollection<IInquirySource> InquirySources { get; }
+
+        /// <summary>
+        /// Гражданство в момент обращения
+        /// </summary>
+        public CitizenshipRecord Сitizenship { get; }
+
+        /// <summary>
+        /// Место проживания в момент обращения + где проиживает
+        /// </summary>
+        public DomicileRecord? Domicile { get; }
+
+        /// <summary>
+        /// Наличие детей
+        /// </summary>
+        public ChildrenRecord? HasChildren { get; }
+
+        /// <summary>
+        /// Уровень образования
+        /// </summary>
+        public IReadOnlyCollection<EducationLevelRecord>? EducationLevel { get; }
+
+        /// <summary>
+        /// Специальности
+        /// </summary>
+        public IReadOnlyCollection<SpecialityRecord>? Specialities { get; }
+
+        /// <summary>
+        /// Опыт работы
+        /// </summary>
+        public string? WorkingExperience { get; }
+
+        /// <summary>
+        /// Иные факторы уязвимости
+        /// </summary>
+        public IReadOnlyCollection<Vulnerability>? VulnerabilityFactors { get; }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="id">идентификатор</param>
+        /// <param name="isDeleted">признак удалённой записи</param>
+        /// <param name="created">дата создания записи</param>
+        /// <param name="lastEdit">дата последнего редактирования</param>
+        /// <param name="documentDate">дата обращения</param>
+        /// <param name="survivor">пострадавший</param>
+        /// <param name="isJuvenile">несовершеннолетний на момент обращения</param>
+        /// <param name="inquirySources">источники обращения</param>
+        /// <param name="сitizenship">гразданство</param>
+        /// <param name="domicile">место жительства, если известно</param>
+        /// <param name="hasChildren">статус по детям</param>
+        /// <param name="educationLevel">статус образования</param>
+        /// <param name="specialities">специальности</param>
+        /// <param name="workingExperience">опыт работы</param>
+        /// <param name="vulnerabilityFactors">факторы уязвимости</param>
+        /// <exception cref="ArgumentNullException">если нет гражданства или источников обращения</exception>
+        public Inquiry(Guid id, bool isDeleted, DateTimeOffset created, DateTimeOffset lastEdit,
+            DateTimeOffset documentDate, Survivor survivor,
+            bool isJuvenile, IEnumerable<IInquirySource> inquirySources, 
+            CitizenshipRecord сitizenship, 
+            DomicileRecord? domicile,
+            ChildrenRecord? hasChildren,
+            IEnumerable<EducationLevelRecord>? educationLevel,
+            IEnumerable<SpecialityRecord>? specialities,
+            string? workingExperience, 
+            IEnumerable<Vulnerability>? vulnerabilityFactors) : 
+            base(id, isDeleted, created, lastEdit, documentDate, survivor)
+        {
+            IsJuvenile = isJuvenile;
+            InquirySources = inquirySources?.ToList() ?? throw new ArgumentNullException(nameof(inquirySources));
+            Сitizenship = сitizenship ?? throw new ArgumentNullException(nameof(сitizenship));
+            Domicile = domicile;
+            HasChildren = hasChildren;
+            EducationLevel = educationLevel?.ToList();
+            Specialities = specialities?.ToList();
+            WorkingExperience = workingExperience;
+            VulnerabilityFactors = vulnerabilityFactors?.ToList();
+        }
+    }
+}
