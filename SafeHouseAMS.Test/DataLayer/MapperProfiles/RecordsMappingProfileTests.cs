3553238d5fad4261c4a05b43564571cfe2128af9
zetroot <zetroot@gmail.com>
2021-07-20 18:52:49 +0300

    records mapping dal -> bl, work is still in progress

---
+++ SafeHouseAMS.Test/DataLayer/MapperProfiles/RecordsMappingProfileTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text.Json;
+using AutoMapper;
+using FluentAssertions;
+using SafeHouseAMS.BizLayer.LifeSituations.Records;
+using SafeHouseAMS.DataLayer.MapperProfiles;
+using SafeHouseAMS.DataLayer.Models.LifeSituations;
+using Xunit;
+using Xunit.Categories;
+
+namespace SafeHouseAMS.Test.DataLayer.MapperProfiles
+{
+    public class RecordsMappingProfileTests
+    {
+        private Mapper BuildMapper()
+        {
+            var cfg = new MapperConfiguration(c => c.AddMaps(typeof(RecordsMappingProfile)));
+            return new(cfg);
+        }
+
+        [Theory, UnitTest]
+        [InlineData(false, null)]
+        [InlineData(false, "")]
+        [InlineData(false, "42")]
+        [InlineData(true, null)]
+        [InlineData(true, "")]
+        [InlineData(true, "42")]
+        public void Mapper_MapsChildrenRecord_Dal2Bl(bool hasChildren, string? details)
+        {
+            //arrange
+            var recordId = Guid.NewGuid();
+            var srcRec = new ChildrenRecord(recordId, hasChildren, details);
+            var src = new ChildrenRecordDAL
+            {
+                ID = recordId,
+                Content = JsonSerializer.Serialize(srcRec)
+            } as BaseRecordDAL;
+            var sut = BuildMapper();
+            
+            //act
+            var result = sut.Map<BaseRecord>(src);
+
+            //assert
+            result.Should().BeOfType<ChildrenRecord>()
+                .And.BeEquivalentTo(srcRec);
+        }
+        
+        [Theory, UnitTest]
+        [InlineData("")]
+        [InlineData("42")]
+        public void Mapper_MapsCitizenshipRecord_Dal2Bl(string citizenship)
+        {
+            //arrange
+            var recordId = Guid.NewGuid();
+            var src = new CitizenshipRecordDAL
+            {
+                ID = recordId,
+                Content = JsonSerializer.Serialize(new CitizenshipRecord(recordId, citizenship))
+            } as BaseRecordDAL;
+            var sut = BuildMapper();
+            
+            //act
+            var result = sut.Map<BaseRecord>(src);
+
+            //assert
+            result.Should().BeOfType<CitizenshipRecord>()
+                .And.BeEquivalentTo(new CitizenshipRecord(recordId, citizenship));
+        }
+        
+        [Theory, UnitTest]
+        [InlineData("", DomicileRecord.PlaceKind.OwnHome, true, true, true, "42", true, "43", true, "44", true, "45")]
+        [InlineData("place", DomicileRecord.PlaceKind.OwnHome, true, true, true, "42", true, "43", true, "44", true, "45")]
+        [InlineData("place", DomicileRecord.PlaceKind.Homeless, true, true, true, "42", true, "43", true, "44", true, "45")]
+        [InlineData("place", DomicileRecord.PlaceKind.Homeless, false, false, false, null, true, null, true, null, true, null)]
+        public void Mapper_MapsDomicileRecord_Dal2Bl(string place, DomicileRecord.PlaceKind? placeKind, 
+            bool livesAlone,
+            bool withPartner,
+            bool withChildren, string? childrenDetails,
+            bool withParents, string? parentsDetails,
+            bool withOtherRelatives, string? otherRelativesDetails, 
+            bool withOtherPeople, string? otherPeopleDetails)
+        {
+            //arrange
+            var recordId = Guid.NewGuid();
+            var recordBl = new DomicileRecord(recordId, place, placeKind, livesAlone, withPartner, withChildren, childrenDetails, 
+            withParents, parentsDetails, withOtherRelatives, otherRelativesDetails, withOtherPeople, otherPeopleDetails);
+            var src = new DomicileRecordDAL()
+            {
+                ID = recordId,
+                Content = JsonSerializer.Serialize(recordBl)
+            } as BaseRecordDAL;
+            var sut = BuildMapper();
+            
+            //act
+            var result = sut.Map<BaseRecord>(src);
+
+            //assert
+            result.Should().BeOfType<DomicileRecord>()
+                .And.BeEquivalentTo(recordBl);
+        }
+    }
+}
---
+++ src/SafeHouseAMS.DataLayer/MapperProfiles/RecordsMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json;
+using AutoMapper;
+using SafeHouseAMS.BizLayer.LifeSituations.Records;
+using SafeHouseAMS.DataLayer.Models.LifeSituations;
+
+namespace SafeHouseAMS.DataLayer.MapperProfiles
+{
+    internal class RecordsMappingProfile : Profile
+    {
+        public RecordsMappingProfile()
+        {
+            MapBaseRecords();
+            MapChildrenRecord();
+        }
+        private void MapChildrenRecord()
+        {
+            CreateMap<ChildrenRecordDAL, ChildrenRecord>()
+                .IncludeBase<BaseRecordDAL, BaseRecord>()
+                .ConstructUsing((src,_) => JsonSerializer.Deserialize<ChildrenRecord>(src.Content) ?? throw new SerializationException());
+        }
+        private void MapBaseRecords()
+        {
+            CreateMap<BaseRecordDAL, BaseRecord>();
+        }
+    }
+}
