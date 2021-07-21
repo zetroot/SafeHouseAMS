0917144b98f5053abfb4039857bc7f1e1aecf281
zetroot <zetroot@gmail.com>
2021-07-21 23:50:21 +0300

    LifeSituationDocumentsRepository GetSingleAsyncTests

---
+++ SafeHouseAMS.Test/DataLayer/Repositories/LifeSituationsRepositoryTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using SafeHouseAMS.BizLayer.LifeSituations;
+using SafeHouseAMS.BizLayer.LifeSituations.Records;
+using SafeHouseAMS.DataLayer;
+using SafeHouseAMS.DataLayer.Models.LifeSituations;
+using SafeHouseAMS.DataLayer.Repositories;
+using Xunit;
+using Xunit.Categories;
+
+namespace SafeHouseAMS.Test.DataLayer.Repositories
+{
+    public class LifeSituationsRepositoryTests
+    {
+        private IMapper CreateMapper()
+        {
+            var cfg = new MapperConfiguration(c => c.AddMaps(typeof(SurvivorsRepository).Assembly));
+            return new Mapper(cfg);
+        }
+        
+        private DataContext CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            var dbctxOptsBuilder = new DbContextOptionsBuilder()
+                .UseLazyLoadingProxies()
+                .UseSqlite(connection, opt => 
+                    opt.MigrationsAssembly(typeof(DataContext).Assembly.FullName));
+            var ctx = new DataContext(dbctxOptsBuilder.Options);
+            ctx.Database.EnsureDeleted();
+            ctx.Database.EnsureCreated();
+            return ctx;
+        }
+
+        [Fact,UnitTest]
+        public void Ctor_WhenDataContextIsNull_Throws() =>
+            Assert.Throws<ArgumentNullException>(() => 
+                new LifeSituationDocumentsRepository(null!, Mock.Of<IMapper>()));
+        
+        [Fact, UnitTest]
+        public void Ctor_WhenMapperIsNull_Throws() =>
+            Assert.Throws<ArgumentNullException>(() => 
+                new LifeSituationDocumentsRepository(new DataContext(new DbContextOptions<DataContext>()), null!));
+
+        
+        [Fact, IntegrationTest]
+        public async Task GetSingleAsync_WhenCalled_ReturnsEntity()
+        {
+            //arrange
+            var id = Guid.NewGuid();
+            await using var ctx = CreateInMemoryDatabase();
+            var survivorId = Guid.NewGuid();
+            await ctx.Survivors.AddAsync(new() {ID = survivorId, Num = 42, Name = "name"});
+            await ctx.LifeSituationDocuments.AddAsync(new InquiryDAL {ID = id, SurvivorID = survivorId});
+            var citizenshipRecord = new CitizenshipRecord(Guid.NewGuid(), "c");
+            await ctx.Records.AddAsync(new CitizenshipRecordDAL {ID = Guid.NewGuid(), Content = JsonSerializer.Serialize(citizenshipRecord), DocumentID = id});
+            await ctx.SaveChangesAsync();
+            var sut = new LifeSituationDocumentsRepository(ctx, CreateMapper());
+            
+            //act
+            var foundRecord = await sut.GetSingleAsync(id, CancellationToken.None);
+            
+            //assert
+            foundRecord.ID.Should().Be(id);
+        }
+        
+        [Fact, IntegrationTest]
+        public async Task GetSingleAsync_WhenRecordIsDeleted_Throws()
+        {
+            //arrange
+            var id = Guid.NewGuid();
+            await using var ctx = CreateInMemoryDatabase();
+
+            var survivorId = Guid.NewGuid();
+            await ctx.Survivors.AddAsync(new() {ID = survivorId, Num = 42, Name = "name"});
+            await ctx.LifeSituationDocuments.AddAsync(new InquiryDAL{ID = id, IsDeleted = true, SurvivorID = survivorId});
+            await ctx.SaveChangesAsync();
+            var sut = new LifeSituationDocumentsRepository(ctx, CreateMapper());
+            
+            //act && assert
+            await Assert.ThrowsAnyAsync<Exception>(() => sut.GetSingleAsync(id, CancellationToken.None));
+        }
+        
+        [Fact, IntegrationTest]
+        public async Task GetSingleAsync_WhenCancelled_ThrowsOperationCancelled()
+        {
+            //arrange
+            await using var ctx = CreateInMemoryDatabase();
+            var sut = new LifeSituationDocumentsRepository(ctx, CreateMapper());
+            var ct = new CancellationToken(true);
+            //act && assert
+            await Assert.ThrowsAnyAsync<OperationCanceledException>(() => sut.GetSingleAsync(default, ct));
+        }
+    }
+}
--- src/SafeHouseAMS.DataLayer/Repositories/LifeSituationDocumentsRepository.cs
+++ src/SafeHouseAMS.DataLayer/Repositories/LifeSituationDocumentsRepository.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using SafeHouseAMS.BizLayer.LifeSituations;
 using SafeHouseAMS.BizLayer.LifeSituations.InquirySources;
 using SafeHouseAMS.BizLayer.LifeSituations.Records;
@@ -11,19 +13,28 @@ namespace SafeHouseAMS.DataLayer.Repositories
     internal class LifeSituationDocumentsRepository : ILifeSituationDocumentsRepository
     {
         private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public LifeSituationDocumentsRepository(DataContext context, IMapper mapper)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
         public async Task<LifeSituationDocument> GetSingleAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var doc = await _context.LifeSituationDocuments
+                .SingleAsync(x => !x.IsDeleted && x.ID == id, cancellationToken);
+            return _mapper.Map<LifeSituationDocument>(doc);
         }
         public IAsyncEnumerable<LifeSituationDocument> GetAllBySurvivor(Guid survivorId, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
-        public async Task CreateInquiry(Guid documentId, bool isDeleted, DateTime created, DateTime lastEdit, Guid survivorID, DateTime documentDate, bool isJuvenile, IEnumerable<IInquirySource> inquirySources)
+        public Task CreateInquiry(Guid documentId, bool isDeleted, DateTime created, DateTime lastEdit, Guid survivorID, DateTime documentDate, bool isJuvenile, IEnumerable<IInquirySource> inquirySources)
         {
             throw new NotImplementedException();
         }
-        public async Task AddRecord(Guid documentId, BaseRecord record)
+        public Task AddRecord(Guid documentId, BaseRecord record)
         {
             throw new NotImplementedException();
         }
