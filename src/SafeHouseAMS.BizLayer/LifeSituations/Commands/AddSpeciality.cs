88906240198864809b7d99da0835e5cfd449da3a
zetroot <zetroot@gmail.com>
2021-07-27 00:32:52 +0300

    added addspeciality command

---
+++ src/SafeHouseAMS.BizLayer/LifeSituations/Commands/AddSpeciality.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using SafeHouseAMS.BizLayer.LifeSituations.Records;
+
+namespace SafeHouseAMS.BizLayer.LifeSituations.Commands
+{
+    /// <summary>
+    /// Команда добавления записи о специальности
+    /// </summary>
+    public class AddSpeciality : LifeSituationDocumentCommand
+    {
+        /// <summary>
+        /// Собственно специальность
+        /// </summary>
+        public string Speciality { get; }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="entityID">идентификатор документ</param>
+        /// <param name="speciality">наименование специальности</param>
+        /// <exception cref="ArgumentNullException">если специальность была null</exception>
+        public AddSpeciality(Guid entityID, string speciality) : base(entityID)
+        {
+            Speciality = speciality ?? throw new ArgumentNullException(nameof(speciality));
+        }
+        internal override Task ApplyOn(ILifeSituationDocumentsRepository repository)
+        {
+            if (repository is null) throw new ArgumentNullException(nameof(repository));
+            return repository.AddRecord(EntityID, new SpecialityRecord(Guid.NewGuid(), Speciality));
+        }
+    }
+}
