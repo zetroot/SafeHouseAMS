cdb66c9cfb3bcd200aa565cc3c97ccb44211bafc
zetroot <zetroot@gmail.com>
2021-07-27 23:09:42 +0300

    added tests for set working experience command

---
+++ tests/SafeHouseAMS.Test/BizLayer/LifeSituations/SetWorkingExperienceCommandTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using SafeHouseAMS.BizLayer.LifeSituations;
+using SafeHouseAMS.BizLayer.LifeSituations.Commands;
+using SafeHouseAMS.BizLayer.LifeSituations.Records;
+using Xunit;
+using Xunit.Categories;
+
+namespace SafeHouseAMS.Test.BizLayer.LifeSituations
+{
+    public class SetWorkingExperienceCommandTests
+    {
+        [Fact, UnitTest]
+        public void Ctor_WhenDetailsIsNull_Throws() =>
+            Assert.Throws<ArgumentNullException>(() => 
+                new SetWorkingExperience(Guid.NewGuid(), null!));
+        
+        [Fact, UnitTest]
+        public void Ctor_Always_SetsProperties()
+        {
+            //arrange
+            var docId = Guid.NewGuid();
+            
+            //act
+            var result = new SetWorkingExperience(docId, "experience");
+            
+            //assert
+            result.EntityID.Should().Be(docId);
+            result.Details.Should().Be("experience");
+        }
+
+        [Fact, UnitTest]
+        public Task ApplyOn_WhenRepositoryIsNull_Throws() =>
+            Assert.ThrowsAsync<ArgumentNullException>(() => 
+                new SetWorkingExperience(Guid.NewGuid(), "details").ApplyOn(null!));
+
+        [Fact, UnitTest]
+        public async Task ApplyOn_WhenCalled_InvokesRepoSetWorkingExp()
+        {
+            //arrange
+            var docId = Guid.NewGuid();
+            const string details = "details";
+            var sut = new SetWorkingExperience(docId, details);
+            var repoMock = new Mock<ILifeSituationDocumentsRepository>();
+            repoMock.Setup(x => x.SetWorkingExperience(It.IsAny<Guid>(), It.IsAny<string>()));
+            
+            //act
+            await sut.ApplyOn(repoMock.Object);
+            
+            //assert
+            repoMock.Verify(x => 
+                x.SetWorkingExperience(docId, It.Is<string>(r => r == details)));
+        }
+    }
+}
