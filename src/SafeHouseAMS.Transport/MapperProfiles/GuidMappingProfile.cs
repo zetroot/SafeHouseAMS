d19633d1b85a25c17cba823b91a329e5e4e8ea93
zetroot <zetroot@gmail.com>
2021-07-30 10:42:56 +0300

    added guid mapping

---
+++ src/SafeHouseAMS.Transport/MapperProfiles/GuidMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AutoMapper;
+using Google.Protobuf;
+using SafeHouseAMS.Backend.Protos.Models.Common;
+
+namespace SafeHouseAMS.Transport.MapperProfiles
+{
+    internal class GuidMappingProfile : Profile
+    {
+        public GuidMappingProfile()
+        {
+            CreateMap<Guid, UUID>()
+                .ConstructUsing(g => new UUID {Value = ByteString.CopyFrom(g.ToByteArray())});
+            
+            CreateMap<UUID, Guid>()
+                .ConstructUsing(g => new Guid(g.Value.ToByteArray()));
+        }
+    }
+}
