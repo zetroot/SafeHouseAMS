2336f39e70e50218b01665cd097adaf33014fa09
zetroot <zetroot@gmail.com>
2021-07-30 20:38:52 +0300

    added serilog to server

--- src/SafeHouseAMS.Backend.Server/Program.cs
+++ src/SafeHouseAMS.Backend.Server/Program.cs
@@ -1,13 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using SafeHouseAMS.BizLayer;
 using SafeHouseAMS.DataLayer;
+using Serilog;
+using Serilog.Events;
 
 namespace SafeHouseAMS.Backend.Server
 {
@@ -21,13 +19,33 @@ namespace SafeHouseAMS.Backend.Server
         /// точка входа в приложение
         /// </summary>
         /// <param name="args">Аргументы запуска</param>
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
