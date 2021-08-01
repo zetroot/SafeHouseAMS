037d5e4f6b6568117ec8ed7b771a220fd68182b3
zetroot <zetroot@gmail.com>
2021-08-01 22:22:15 +0300

    added mapping and tests

---
+++ src/SafeHouseAMS.Transport/MapperProfiles/InquiryCommandsMappingProfile.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using SafeHouseAMS.BizLayer.LifeSituations.Commands;
+
+namespace SafeHouseAMS.Transport.MapperProfiles
+{
+    internal class InquiryCommandsMappingProfile : Profile
+    {
+        public InquiryCommandsMappingProfile()
+        {
+            MapAddEducationLevelCommand();
+            MapAddSpeciality();
+            MapCreateInquiry();
+            MapSetChildren();
+            MapSetDomicile();
+            MapSetVulnerabilities();
+            MapSetWorkingExperience();
+        }
+        private void MapSetWorkingExperience()
+        {
+            CreateMap<SetWorkingExperience, Protos.Models.LifeSituations.Commands.SetWorkingExperience>();
+            CreateMap<Protos.Models.LifeSituations.Commands.SetWorkingExperience, SetWorkingExperience>();
+        }
+        private void MapSetVulnerabilities()
+        {
+            CreateMap<SetVulnerabilities, Protos.Models.LifeSituations.Commands.SetVulnerabilities>();
+            CreateMap<Protos.Models.LifeSituations.Commands.SetVulnerabilities, SetVulnerabilities>();
+        }
+        private void MapSetDomicile()
+        {
+            CreateMap<SetDomicile, Protos.Models.LifeSituations.Commands.SetDomicile>();
+            CreateMap<Protos.Models.LifeSituations.Commands.SetDomicile, SetDomicile>();
+        }
+        private void MapSetChildren()
+        {
+            CreateMap<SetChildren, Protos.Models.LifeSituations.Commands.SetChildren>();
+            CreateMap<Protos.Models.LifeSituations.Commands.SetChildren, SetChildren>();
+        }
+        private void MapCreateInquiry()
+        {
+            CreateMap<CreateInquiry, Protos.Models.LifeSituations.Commands.CreateInquiry>();
+            CreateMap<Protos.Models.LifeSituations.Commands.CreateInquiry, CreateInquiry>();
+        }
+        private void MapAddSpeciality()
+        {
+            CreateMap<AddSpeciality, Protos.Models.LifeSituations.Commands.AddSpeciality>();
+            CreateMap<Protos.Models.LifeSituations.Commands.AddSpeciality, AddSpeciality>();
+        }
+        private void MapAddEducationLevelCommand()
+        {
+            CreateMap<AddEducationLevel, Protos.Models.LifeSituations.Commands.AddEducationLevel>();
+            CreateMap<Protos.Models.LifeSituations.Commands.AddEducationLevel, AddEducationLevel>();
+        }
+    }
+}
--- tests/SafeHouseAMS.Test/Transport/MapperProfiles/InquiryMappingTests.cs
+++ tests/SafeHouseAMS.Test/Transport/MapperProfiles/InquiryMappingTests.cs
@@ -1,13 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using AutoMapper;
+﻿using AutoMapper;
 using FluentAssertions;
 using FsCheck;
 using FsCheck.Xunit;
 using SafeHouseAMS.BizLayer.LifeSituations;
-using SafeHouseAMS.BizLayer.LifeSituations.InquirySources;
-using SafeHouseAMS.BizLayer.LifeSituations.Records;
-using SafeHouseAMS.BizLayer.LifeSituations.Vulnerabilities;
 using SafeHouseAMS.Transport.MapperProfiles;
 
 namespace SafeHouseAMS.Test.Transport.MapperProfiles
@@ -38,33 +33,4 @@ namespace SafeHouseAMS.Test.Transport.MapperProfiles
         }
     }
 
-    public class InquiryGenerators
-    {
-        private static Gen<Vulnerability?> addictionGen = Arb.From<Addiction>().Generator.Select(x => x as Vulnerability).Zip(Arb.From<bool>().Generator).Select(x => x.Item2? x.Item1 : null);
-        private static Gen<Vulnerability?> childhoodViolenceGen = Arb.From<ChildhoodViolence>().Generator.Select(x => x as Vulnerability).Zip(Arb.From<bool>().Generator).Select(x => x.Item2? x.Item1 : null);
-        private static Gen<Vulnerability?> homelessGen = Arb.From<Homelessness>().Generator.Select(x => x as Vulnerability).Zip(Arb.From<bool>().Generator).Select(x => x.Item2? x.Item1 : null);
-        private static Gen<Vulnerability?> migrationGen = Arb.From<Migration>().Generator.Select(x => x as Vulnerability).Zip(Arb.From<bool>().Generator).Select(x => x.Item2? x.Item1 : null);
-        private static Gen<Vulnerability?> orphExpGen = Arb.From<OrphanageExperience>().Generator.Select(x => x as Vulnerability).Zip(Arb.From<bool>().Generator).Select(x => x.Item2? x.Item1 : null);
-        private static Gen<Vulnerability?> otherGen = Arb.From<Other>().Generator.Select(x => x as Vulnerability).Zip(Arb.From<bool>().Generator).Select(x => x.Item2? x.Item1 : null);
-        private static Gen<Vulnerability?> healthGen = Arb.From<HealthStatus>().Generator.Select(x => x as Vulnerability).Zip(Arb.From<bool>().Generator).Select(x => x.Item2? x.Item1 : null);
-
-        private static Gen<IInquirySource> selfInquriyGen = Arb.From<SelfInquiry>().Generator.Select(x => x as IInquirySource);
-        private static Gen<IInquirySource> forwardSurvivorGen = Arb.From<ForwardedBySurvivor>().Generator.Select(x => x as IInquirySource);
-        private static Gen<IInquirySource> forwardPersonGen = Arb.From<ForwardedByPerson>().Generator.Select(x => x as IInquirySource);
-        private static Gen<IInquirySource> forwardOrganizationGen = Arb.From<ForwardedByOrganization>().Generator.Select(x => x as IInquirySource);
-
-        public static Arbitrary<IEnumerable<IInquirySource>> InquirySourcesArb => Gen
-            .OneOf(selfInquriyGen, forwardSurvivorGen, forwardPersonGen, forwardOrganizationGen).ListOf().Select(x => x.AsEnumerable()).ToArbitrary();
-        
-        public static Arbitrary<IEnumerable<Vulnerability>> Vulnerabilities => Gen
-            .Sequence(addictionGen, childhoodViolenceGen, homelessGen, migrationGen, orphExpGen, otherGen, healthGen)
-            .Select(x => x.Where(y => y is not null).Select(y => y!))
-            .ToArbitrary();
-
-        public static Arbitrary<IEnumerable<EducationLevelRecord>> EducationLevels => Arb.From<EducationLevelRecord>().Generator.ListOf().Select(x => x.AsEnumerable()).ToArbitrary();
-        public static Arbitrary<IEnumerable<SpecialityRecord>> Specialities => Arb.From<SpecialityRecord>().Generator.ListOf().Select(x => x.AsEnumerable()).ToArbitrary();
-        public static Arbitrary<IEnumerable<DomicileRecord>> Domiciles => Arb.From<DomicileRecord>().Generator.ListOf().Select(x => x.AsEnumerable()).ToArbitrary();
-        public static Arbitrary<IEnumerable<ChildrenRecord>> Childrens => Arb.From<ChildrenRecord>().Generator.ListOf().Select(x => x.AsEnumerable()).ToArbitrary();
-        public static Arbitrary<IEnumerable<CitizenshipRecord>> Citizenship => Arb.From<CitizenshipRecord>().Generator.ListOf().Select(x => x.AsEnumerable()).ToArbitrary();
-    }
 }
