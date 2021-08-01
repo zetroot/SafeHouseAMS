69b15712d7dfcd3b448bd9fda61037d2f75c7c98
zetroot <zetroot@gmail.com>
2021-08-01 16:49:05 +0300

    added inquiry sources mapping and tests

---
+++ src/SafeHouseAMS.Transport/MapperProfiles/InquiryMappingProfile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using AutoMapper;
+using SafeHouseAMS.BizLayer.LifeSituations.InquirySources;
+
+namespace SafeHouseAMS.Transport.MapperProfiles
+{
+    internal class InquiryMappingProfile : Profile
+    {
+        public InquiryMappingProfile()
+        {
+            MapInquirySources();
+        }
+        private void MapInquirySources()
+        {
+            CreateMap<SelfInquiry, Protos.Models.LifeSituations.SelfInquiry>();
+            CreateMap<Protos.Models.LifeSituations.SelfInquiry, SelfInquiry>();
+            
+            CreateMap<ForwardedBySurvivor, Protos.Models.LifeSituations.ForwardedBySurvivor>();
+            CreateMap<Protos.Models.LifeSituations.ForwardedBySurvivor, ForwardedBySurvivor>();
+            
+            CreateMap<ForwardedByPerson, Protos.Models.LifeSituations.ForwardedByPerson>();
+            CreateMap<Protos.Models.LifeSituations.ForwardedByPerson, ForwardedByPerson>();
+            
+            CreateMap<ForwardedByOrganization, Protos.Models.LifeSituations.ForwardedByOrganization>();
+            CreateMap<Protos.Models.LifeSituations.ForwardedByOrganization, ForwardedByOrganization>();
+
+            CreateMap<IInquirySource, Protos.Models.LifeSituations.InquirySource>()
+                .ConstructUsing((src, ctx) =>
+                {
+                    var result = new Protos.Models.LifeSituations.InquirySource();
+                    switch (src)
+                    {
+                        case SelfInquiry selfInquiry:
+                            result.SelfInquiry = ctx.Mapper.Map<Protos.Models.LifeSituations.SelfInquiry>(selfInquiry);
+                            break;
+                        case ForwardedBySurvivor forwardedBySurvivor:
+                            result.ForwardedBySurvivor = ctx.Mapper.Map<Protos.Models.LifeSituations.ForwardedBySurvivor>(forwardedBySurvivor);
+                            break;
+                        case ForwardedByPerson forwardedByPerson:
+                            result.ForwardedByPerson = ctx.Mapper.Map<Protos.Models.LifeSituations.ForwardedByPerson>(forwardedByPerson);
+                            break;
+                        case ForwardedByOrganization forwardedByOrganization:
+                            result.ForwardedByOrganization = ctx.Mapper.Map<Protos.Models.LifeSituations.ForwardedByOrganization>(forwardedByOrganization);
+                            break;
+                        default:
+                            throw new InvalidOperationException();
+                    }
+                    return result;
+                });
+            
+            CreateMap<Protos.Models.LifeSituations.InquirySource, IInquirySource>()
+                .ConstructUsing((src, ctx) => src.KindCase switch
+                {
+                    Protos.Models.LifeSituations.InquirySource.KindOneofCase.SelfInquiry => ctx.Mapper.Map<SelfInquiry>(src.SelfInquiry),
+                    Protos.Models.LifeSituations.InquirySource.KindOneofCase.ForwardedBySurvivor => ctx.Mapper.Map<ForwardedBySurvivor>(src.ForwardedBySurvivor),
+                    Protos.Models.LifeSituations.InquirySource.KindOneofCase.ForwardedByPerson => ctx.Mapper.Map<ForwardedByPerson>(src.ForwardedByPerson),
+                    Protos.Models.LifeSituations.InquirySource.KindOneofCase.ForwardedByOrganization => ctx.Mapper.Map<ForwardedByOrganization>(src.ForwardedByOrganization),
+                    _ => throw new InvalidOperationException()
+                });
+        }
+    }
+}
---
+++ tests/SafeHouseAMS.Test/Transport/MapperProfiles/InquirySourcesMappingTests.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using AutoMapper;
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
+using SafeHouseAMS.BizLayer.LifeSituations.InquirySources;
+using SafeHouseAMS.Transport.MapperProfiles;
+
+namespace SafeHouseAMS.Test.Transport.MapperProfiles
+{
+    public class InquirySourcesMappingTests
+    {
+        private Mapper BuildMapper()
+        {
+            var cfg = new MapperConfiguration(c => c.AddMaps(typeof(InquiryMappingProfile).Assembly));
+            return new(cfg);
+        }
+
+        [Property]
+        public void SelfInquiry_RoundTrip_DoesNotChanges()
+        {
+            var mapper = BuildMapper();
+            Prop.ForAll<SelfInquiry>(src =>
+            {
+                var dto = mapper.Map<SafeHouseAMS.Transport.Protos.Models.LifeSituations.SelfInquiry>(src);
+                var result = mapper.Map<SelfInquiry>(dto);
+                
+                result.Should().BeEquivalentTo(src);
+            }).VerboseCheckThrowOnFailure();
+        }
+        
+        [Property]
+        public void ForwardedBySurvivor_RoundTrip_DoesNotChanges()
+        {
+            var mapper = BuildMapper();
+            Arb.Register<NotNullStringsGenerators>();
+            Prop.ForAll<ForwardedBySurvivor>(src =>
+            {
+                var dto = mapper.Map<SafeHouseAMS.Transport.Protos.Models.LifeSituations.ForwardedBySurvivor>(src);
+                var result = mapper.Map<ForwardedBySurvivor>(dto);
+                
+                result.Should().BeEquivalentTo(src);
+            }).VerboseCheckThrowOnFailure();
+        }
+        
+        [Property]
+        public void ForwardedByPerson_RoundTrip_DoesNotChanges()
+        {
+            var mapper = BuildMapper();
+            Arb.Register<NotNullStringsGenerators>();
+            Prop.ForAll<ForwardedByPerson>(src =>
+            {
+                var dto = mapper.Map<SafeHouseAMS.Transport.Protos.Models.LifeSituations.ForwardedByPerson>(src);
+                var result = mapper.Map<ForwardedByPerson>(dto);
+                
+                result.Should().BeEquivalentTo(src);
+            }).VerboseCheckThrowOnFailure();
+        }
+        
+        [Property]
+        public void ForwardedByOrganization_RoundTrip_DoesNotChanges()
+        {
+            var mapper = BuildMapper();
+            Arb.Register<NotNullStringsGenerators>();
+            Prop.ForAll<ForwardedByOrganization>(src =>
+            {
+                var dto = mapper.Map<SafeHouseAMS.Transport.Protos.Models.LifeSituations.ForwardedByOrganization>(src);
+                var result = mapper.Map<ForwardedByOrganization>(dto);
+                
+                result.Should().BeEquivalentTo(src);
+            }).VerboseCheckThrowOnFailure();
+        }
+
+        [Property]
+        public void IInquirySourceCollection_RoundTrip_DoesNotChanges()
+        {
+            Arb.Register<NotNullStringsGenerators>();
+            var selfInquriyGen = Arb.From<SelfInquiry>().Generator.Select(x => x as IInquirySource);
+            var forwardSurvivorGen = Arb.From<ForwardedBySurvivor>().Generator.Select(x => x as IInquirySource);
+            var forwardPersonGen = Arb.From<ForwardedByPerson>().Generator.Select(x => x as IInquirySource);
+            var forwardOrganizationGen = Arb.From<ForwardedByOrganization>().Generator.Select(x => x as IInquirySource);
+
+            var inquirySourcesArb = Gen.OneOf(selfInquriyGen, forwardSurvivorGen, forwardPersonGen, forwardOrganizationGen).ListOf().ToArbitrary();
+            
+            var mapper = BuildMapper();
+            
+
+            Prop.ForAll(inquirySourcesArb, src =>
+            {
+                var dto = src.Select(mapper.Map<SafeHouseAMS.Transport.Protos.Models.LifeSituations.InquirySource>);
+                var result = dto.Select(mapper.Map<IInquirySource>);
+
+                result.Should().BeEquivalentTo(src, opt => opt.RespectingRuntimeTypes());
+            }).VerboseCheckThrowOnFailure();
+        } 
+    }
+}
