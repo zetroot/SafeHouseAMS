836a5a9c9462b508d3e819a70538a1f75a8e4a64
zetroot <zetroot@gmail.com>
2021-08-06 09:42:08 +0300

    MigrationStatusRecord with tests

---
+++ src/SafeHouseAMS.BizLayer/LifeSituations/Records/MigrationStatusRecord.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace SafeHouseAMS.BizLayer.LifeSituations.Records
+{
+    /// <summary>
+    /// Запись о миграционном статусе
+    /// </summary>
+    public class MigrationStatusRecord : BaseRecord
+    {
+        /// <summary>
+        /// собственно информация о миграционном статусе
+        /// </summary>
+        public string Details { get; }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="id">идентификатор записи</param>
+        /// <param name="details">информация о миграционном статусе</param>
+        /// <exception cref="ArgumentNullException">если передали пустую строку</exception>
+        public MigrationStatusRecord(Guid id, string details) : base(id)
+        {
+            Details = details ?? throw new ArgumentNullException(nameof(details));
+        }
+    }
+}
