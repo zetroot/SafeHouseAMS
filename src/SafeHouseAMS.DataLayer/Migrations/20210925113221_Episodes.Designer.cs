dff6f8efdbed7a60f7c16f33d6d9d839d8c2063f
zetroot <zetroot@gmail.com>
2021-09-25 14:33:33 +0300

    migration added

--- src/SafeHouseAMS.DataLayer/Migrations/20210925113221_Episodes.Designer.cs
+++ src/SafeHouseAMS.DataLayer/Migrations/20210925113221_Episodes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SafeHouseAMS.DataLayer;
@@ -9,9 +10,10 @@ using SafeHouseAMS.DataLayer;
 namespace SafeHouseAMS.DataLayer.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210925113221_Episodes")]
+    partial class Episodes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
