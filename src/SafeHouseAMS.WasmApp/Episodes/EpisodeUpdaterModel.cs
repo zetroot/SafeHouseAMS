7b989c9c46b22cdee9857825714e42b3fc943098
zetroot <zetroot@gmail.com>
2021-10-05 18:52:23 +0300

    added edit episode page

--- src/SafeHouseAMS.WasmApp/Episodes/EpisodeUpdaterModel.cs
+++ src/SafeHouseAMS.WasmApp/Episodes/EpisodeUpdaterModel.cs
@@ -14,6 +14,7 @@ namespace SafeHouseAMS.WasmApp.Episodes
         public EpisodeUpdaterModel(Episode origin) : base()
         {
             this.origin = origin;
+
             Involvement = origin.ContactReason.Involvement is not null;
             InvolvementDescription = origin.ContactReason.Involvement?.Details ?? string.Empty;
 
@@ -26,6 +27,64 @@ namespace SafeHouseAMS.WasmApp.Episodes
                     item.Selected = true;
             }
 
+            ForcedLabour = origin.ContactReason.ForcedLabour is not null;
+            ForcedLabourDescription = origin.ContactReason.ForcedLabour?.Details ?? string.Empty;
+            foreach (var item in ForcedLabourType)
+            {
+                if (origin.ContactReason.ForcedLabour is null) break;
+                if (origin.ContactReason.ForcedLabour.Type.HasFlag(item.Item))
+                    item.Selected = true;
+            }
+
+            ForcedMarriage = origin.ContactReason.ForcedMarriage is not null;
+            ForcedMarriageDescription = origin.ContactReason.ForcedMarriage?.Details ?? string.Empty;
+            ForcedMarriageKind = origin.ContactReason.ForcedMarriage?.Type ?? default;
+
+            Cre = origin.ContactReason.Cre is not null;
+            CreDescription = origin.ContactReason.Cre?.Details ?? string.Empty;
+
+            Begging = origin.ContactReason.Begging is not null;
+            BeggingDescription = origin.ContactReason.Begging?.Details ?? string.Empty;
+
+            ForcedCriminalActivity = origin.ContactReason.ForcedCriminalActivity is not null;
+            CriminalActivityDescription = origin.ContactReason.ForcedCriminalActivity?.Details ?? string.Empty;
+            foreach (var item in CriminalActivityKind)
+            {
+                if (origin.ContactReason.ForcedCriminalActivity is null) break;
+                if (origin.ContactReason.ForcedCriminalActivity.Type.HasFlag(item.Item))
+                    item.Selected = true;
+            }
+
+            OtherExploitationKind = origin.ContactReason.OtherExploitationKind is not null;
+            OtherExploitationKindDescription = origin.ContactReason.OtherExploitationKind?.Details ?? string.Empty;
+
+            SexualViolence = origin.ContactReason.SexualViolence is not null;
+            SexualViolenceDescription = origin.ContactReason.SexualViolence?.Details ?? string.Empty;
+
+            DomesticViolence = origin.ContactReason.DomesticViolence is not null;
+            DomesticViolenceDescription = origin.ContactReason.DomesticViolence?.Details ?? string.Empty;
+
+            OtherViolence = origin.ContactReason.DomesticViolence is not null;
+            OtherViolenceDescription = origin.ContactReason.DomesticViolence?.Details ?? string.Empty;
+
+            Place = origin.Place;
+            InvolvementDescriptionText = origin.InvolvementDescription;
+            WasJuvenile = origin.WasJuvenile;
+
+            // TODO: add duration parsing
+
+            foreach (var item in ControlMethodKinds)
+            {
+                if (origin.ControlMethods.Methods.HasFlag(item.Item))
+                    item.Selected = true;
+            }
+            foreach (var item in DebtKinds)
+            {
+                if (origin.ControlMethods.DebtKind is null) break;
+                if (origin.ControlMethods.DebtKind.Value.HasFlag(item.Item))
+                    item.Selected = true;
+            }
+            OtherControlMethods = origin.ControlMethods.OtherDetails ?? string.Empty;
         }
 
         public UpdateEpisode BuildCommand()
