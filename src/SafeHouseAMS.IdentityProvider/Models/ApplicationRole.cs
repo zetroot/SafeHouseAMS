4b877d819d5c0f26c84b968838e5874db9b018e0
zetroot <zetroot@gmail.com>
2021-10-17 17:19:52 +0300

    tidying up

---
+++ src/SafeHouseAMS.IdentityProvider/Models/ApplicationRole.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace SafeHouseAMS.IdentityProvider.Models
+{
+    /// <summary>
+    /// Роль пользователя в приложении
+    /// </summary>
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+
+    }
+}
--- src/SafeHouseAMS.IdentityProvider/Models/ApplicationUser.cs
+++ src/SafeHouseAMS.IdentityProvider/Models/ApplicationUser.cs
@@ -3,13 +3,11 @@ using Microsoft.AspNetCore.Identity;
 
 namespace SafeHouseAMS.IdentityProvider.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
+    /// <summary>
+    /// Пользователь приложения
+    /// </summary>
     public class ApplicationUser : IdentityUser<Guid>
     {
     }
 
-    public class ApplicationRole : IdentityRole<Guid>
-    {
-
-    }
 }
