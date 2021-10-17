e99bfdd9ace8860d2543d33ed24c3e475f293f5d
zetroot <zetroot@gmail.com>
2021-10-17 15:22:58 +0300

    added wasm client definition

--- src/SafeHouseAMS.IdentityProvider/Config.cs
+++ src/SafeHouseAMS.IdentityProvider/Config.cs
@@ -26,33 +26,18 @@ namespace SafeHouseAMS.IdentityProvider
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-                // m2m client credentials flow client
-                new Client
+                new ()
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
+                    ClientId = "blazor",
                     AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowedCorsOrigins = {"https://localhost:5001"},
+                    AllowedScopes = {"openid", "profile"},
+                    RedirectUris = {"https://localhost:5001/authentication/login-callback"},
+                    PostLogoutRedirectUris = {"https://localhost:5001"},
+                    Enabled = true
+                }
             };
     }
 }
