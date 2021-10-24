cfb958bbc90ae5b60b0dd2913ebcba94108f387c
ArtemK123 <darkmode1012@gmail.com>
2021-10-25 00:01:00 +0300

    Implemented GetAll and Delete endpoints in IdentityService

--- src/SafeHouseAMS.IdentityProvider/Controllers/API/UsersApiController.cs
+++ src/SafeHouseAMS.IdentityProvider/Controllers/API/UsersApiController.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using IdentityServer4;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SafeHouseAMS.IdentityProvider.Models;
 
 namespace SafeHouseAMS.IdentityProvider.Controllers.API
@@ -18,6 +20,10 @@ namespace SafeHouseAMS.IdentityProvider.Controllers.API
         {
             _userManager = userManager;
         }
+
+        [HttpGet]
+        public async Task<IReadOnlyCollection<UserApiModel>> GetAll()
+            => await _userManager.Users.Select(user => new UserApiModel(user.Id, user.FirstName, user.LastName, user.UserName)).ToArrayAsync();
 
         [HttpPost]
         public async Task<IActionResult> Register([FromBody]RegisterUserRequestModel request)
@@ -35,6 +41,29 @@ namespace SafeHouseAMS.IdentityProvider.Controllers.API
                 UserName = $"{request.FirstName}_{request.LastName}"
             };
             IdentityResult creationResult = await _userManager.CreateAsync(user, request.Password);
+            if (creationResult.Succeeded)
+            {
+                return Ok();
+            }
+
+            return BadRequest(string.Join(", ", creationResult.Errors.Select(error => $"{error.Code}: {error.Description}")));
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> Delete([FromRoute]string userId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            ApplicationUser userToDelete = await _userManager.FindByIdAsync(userId);
+            if (userToDelete is null)
+            {
+                return BadRequest("User is not found");
+            }
+
+            IdentityResult creationResult = await _userManager.DeleteAsync(userToDelete);
             if (creationResult.Succeeded)
             {
                 return Ok();
